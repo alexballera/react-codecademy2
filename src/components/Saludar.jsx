5b580017fd8d27ab8affd1fdaee34ccc6c54721e
Alex Ballera <alexballera@gmail.com>
2017-03-27 18:40:51 -0400

    Estructurando e instalando npm

--- src/components/Saludar.jsx
+++
@@ -1,9 +0,0 @@
-'use strict'
-import React from 'react'
-
-var Saludar = React.createClass({
-  render: function () {
-    return <h1>Saludos usando Props!, Hola {this.props.nombre}</h1>
-  }
-})
-module.exports = Saludar
