8215854fdc250cbec2d2ad0fe14ec831e5f67fc6
Alex Ballera <alexballera@gmail.com>
2017-03-29 01:16:51 -0400

    Agregando estilo desde archivo jsx

--- src/components/Sibling.jsx
+++ src/components/Sibling.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import {Row} from 'react-materialize'
+import style from './Style.jsx'
 
 var Sibling = React.createClass({
   render: function () {
@@ -11,11 +12,16 @@ var Sibling = React.createClass({
       color: '#2196f3',
       marginTop: 50 // por defecto no se coloca px
     }
+
+    var styleH2 = {
+      backgroundColor: style.blue,
+      color: style.red
+    }
     return (
       <Row>
         <h1 style={{color: '#8e24aa'}}>Mi nombre es {name}</h1>
         <h2 style={styles}>Don't you think {name} is the prettiest name ever?</h2>
-        <h2>Sure am glad that my parents picked {name}!</h2>
+        <h2 style={styleH2}>Sure am glad that my parents picked {name}!</h2>
       </Row>
     )
   }
---
+++ src/components/Style.jsx
@@ -0,0 +1,7 @@
+var blue = '#2196f3'
+var red = '#d81b60'
+
+module.exports = {
+  blue: blue,
+  red: red
+}
