ef1b3546eb37f27d566ac07f7c03bf30e6568626
Alex Ballera <alexballera@gmail.com>
2017-03-29 00:52:23 -0400

    Agregado estilos con variable

--- src/components/Sibling.jsx
+++ src/components/Sibling.jsx
@@ -6,10 +6,14 @@ import {Row} from 'react-materialize'
 var Sibling = React.createClass({
   render: function () {
     var name = this.props.name
+    var styles = {
+      backgroundColor: '#d81b60',
+      color: '#2196f3'
+    }
     return (
       <Row>
         <h1 style={{color: '#8e24aa'}}>Mi nombre es {name}</h1>
-        <h2>Don't you think {name} is the prettiest name ever?</h2>
+        <h2 style={styles}>Don't you think {name} is the prettiest name ever?</h2>
         <h2>Sure am glad that my parents picked {name}!</h2>
       </Row>
     )
